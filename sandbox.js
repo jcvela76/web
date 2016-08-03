5f45bdbb60c866493e7d8a20795d299930e8e59a
jcvela76 <jcvela@gmail.com>
2016-08-03 12:05:23 -0400

    Menu animation 1
    Created the first animation to menu

--- sandbox.js
+++ sandbox.js
@@ -10,7 +10,7 @@ angular.module('MyApp', [])
   self.videoHeight = 1080;
   self.scrollPos = 0;
   //self.skill_i = [" Developer;", "Email marketing;", "Web Troubleshooting;", "Web Consulting;"];
-  self.skill_i = [" test;", "2;", "2;", "3;"];
+  self.skill_i = [" test;", "2;", "2;", "Web Consulting;"];
   self.skill = "";
   self.promt = "_";
   self.control_promt = 1;
@@ -34,21 +34,11 @@ angular.module('MyApp', [])
   $timeout(function() {
       
       angular.element(scroll_sign).css( "display", "block");
+      
       self.control_scroll = true;
 
   },self.skill_i[0].length * 500 + self.skill_i[1].length * 500 + self.skill_i[2].length * 500 + self.skill_i[3].length * 500 + 3000);
 
-
-
-  $interval(function() {
-    self.control_promt = self.control_promt * -1;
-    if (self.control_promt > 0){
-        angular.element(promt_).css( "opacity", 1);
-    }
-    else{
-        angular.element(promt_).css( "opacity", 0);
-    }
-  }, 500);//blink promt
 
 
   function skills_presentation (skill,pass) {
@@ -80,10 +70,57 @@ angular.module('MyApp', [])
         video[0].pause();      
         angular.element(vid).css( "opacity", 0.4);      
         angular.element(scroll_sign).css( "display", "none");
+
+
+        angular.element(menu1).css( "display", "inline-block");
+        angular.element(menu2).css( "display", "inline-block");
+        angular.element(menu3).css( "display", "inline-block");
+        angular.element(menu4).css( "display", "inline-block");   
+        angular.element(layer_intro).css( "padding-top", "280px");   
+        self.control_scroll = false;   
       }
  
   });//scroll
 
+  self.isActive = false;
+  self.isActive_menu_1 = false;
+  self.isActive_menu_2 = false;
+  self.isActive_menu_3 = false;
+  self.isActive_menu_4 = false;
+
+  self.open_menu_1 = function(e) {
+    if (!self.isActive){
+      self.isActive = !self.isActive;
+      self.isActive_menu_1 = true;
+      $document.find("body").css('overflow-y', 'auto');
+    }
+    
+  };
+
+  self.open_menu_2 = function(e) {
+  if (!self.isActive){
+      self.isActive = !self.isActive;
+      self.isActive_menu_2 = true;
+      $document.find("body").css('overflow-y', 'auto');
+      angular.element(menu1).css( "display", "none");
+    }
+    
+  };
+
+
+  self.close_menu = function(e) {
+    if (self.isActive){
+      self.isActive = !self.isActive;
+      self.isActive_menu_1 = false;
+      self.isActive_menu_2 = false;
+      angular.element(menu1).css( "display", "inline-block");
+      angular.element(menu2).css( "display", "inline-block");
+      angular.element(menu3).css( "display", "inline-block");
+      angular.element(menu4).css( "display", "inline-block"); 
+      $document.find("body").css('overflow-y', 'hidden');
+    }
+    
+  };
 
   //angular.element('#layer-background').addClass('hide');
   //$document.find(layer).css( "display", "none" );
